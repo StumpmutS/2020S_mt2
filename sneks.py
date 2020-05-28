10e797aa5e75e207e8bbe8510b5af834fcc3b5ce
Michal Young <michal.young@gmail.com>
2020-05-28 08:18:07 -0700

    Added recursion problem: Snakes in a tree

---
+++ sneks.py
@@ -0,0 +1,40 @@
+"""Snakes in a tree:
+They can be anywhere in the tree.  And I really
+like snakes, so I want to know how many snakes
+I can find on one path from root to leaf.
+"""
+
+from typing import List
+
+class SnakeTree:
+    def max_snakes_path(self) -> int:
+        raise NotImplementedError("Help me look for snakes!")
+
+class Leaf(SnakeTree):
+    def __init__(self, snakes: int):
+        self._snakes = snakes
+
+    def max_snakes_path(self) -> int:
+        return 99  # FIXME
+
+class Branch(SnakeTree):
+    def __init__(self, snakes: int, children: List[SnakeTree]):
+        self._snakes = snakes
+        self._children = children
+
+    def max_snakes_path(self) -> int:
+        return 99 # FIXME
+
+def main():
+    """Smoke test"""
+    tree = Branch(3, [
+                Branch(2, [
+                    Leaf(4), Leaf(2)
+                ]),
+                Leaf(8)
+            ])
+    # Max should be 11?
+    print(tree.max_snakes_path())
+
+if __name__ == "__main__":
+    main()
